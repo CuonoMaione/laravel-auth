41bd62b9b705a91b006776cebf2eff987ef0e017
CuonoMaione <cuonomaione99@outlook.it>
2023-08-24 22:43:54 +0200

    add edit and update cruds

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
@@ -53,18 +54,30 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Post $post)
     {
-        //
+
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request,Post $post)
     {
-        //
+        $data = $request->validate([
+            'title' => ['required' , 'max:255', Rule::unique('posts')->ignore($post->id)],
+            'content' => ['required', 'min:10' ],
+            'image' => ['url:https'],
+        ]);
+
+        $data['slug'] = Str::of($data['title'])->slug('-');
+
+        $post->update($data);
+
+        return redirect()->route('admin.posts.show',compact('post'));
     }
+
 
     /**
      * Remove the specified resource from storage.
